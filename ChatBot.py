6c593f4e3292478d116301a262368a8154724176
MangoGab <gabrielhmango@gmail.com>
2023-07-10 21:47:29 -0300

    Adicionados setters e getters a ChatBot e Pairs

--- ChatBot.py
+++ ChatBot.py
@@ -1,4 +1,5 @@
 from nltk.chat.util import Chat
+import os
 
 reflections_pt = {'eu': 'você',
                   'eu sou': 'você é',
@@ -12,66 +13,59 @@ reflections_pt = {'eu': 'você',
                   "você irá": 'eu irei',
                   'seu': 'meu'}
 
-# pairs_pt = [
-#     [
-#      r'oi|olá|opa',
-#      ['olá', 'como vai?', 'tudo bem?']
-#     ],
-#     [
-#      r'qual é o seu nome?',
-#      ['Meu nome é Chat e eu sou um chatbot']
-#     ],  
-#     [
-#      r'(.*) idade?',
-#      ['Não tenho idade pois sou um chatbot']
-#     ], 
-#     [
-#      r'meu nome é (.*)',
-#      ['Olá %1, como você está hoje?']
-#     ],  
-#     [
-#      r'eu trabalho na empresa (.*)',
-#      ['Eu conheço a empresa %1']
-#     ], 
-#     [
-#      r'(.*) (cidade|país)',
-#      ['Porto União, Brasil']
-#     ], 
-#     [
-#      r'quit',
-#      ['Até breve', 'Foi bom conversar com você. Até breve!']
-#     ]     
-# ]
-
-
 
 class ChatBot(Chat):
 
     def __init__(self,nome, pairs, reflections= reflections_pt):
         super().__init__(pairs, reflections)
         self._nome = nome
+    
+    @property
+    def nome(self):
+        return self._nome
+    
+    @nome.setter
+    def nome(self, value):
+        self._nome = value
 
 
 
 class Pairs():
     
-    def __init__(self, nome = None):
+    def __init__(self, nome = None, pairs = []):
         self._nome = nome
-        self.pairs = []
+        self._pairs = pairs
         self.adiconar_conversa('sair', 'Até breve')
     
     def adiconar_conversa(self, pergunta, resposta): 
-        self.pairs.append(self._criar_conversa(pergunta, resposta))
+        self._pairs.append(self._criar_conversa(pergunta, resposta))
 
     def _criar_conversa(self, _pergunta, _resposta):
         return [r''.join(_pergunta), _resposta]
     
-    def retorna_pairs(self):
-        return self.pairs
+    @property
+    def nome(self):
+        return self._nome
+    
+    @nome.setter
+    def nome(self, value):
+        self._nome = value
+    
+    @property
+    def pairs(self):
+        return self._pairs
     
 
 if __name__ == '__main__':
+    os.system('cls')
+
     pairs_teste = Pairs('Teste')
     pairs_teste.adiconar_conversa('Olá', 'Bem-vindo! Em que posso ti ajudar?')
+    print(pairs_teste.pairs)
     
-    chat = ChatBot('Teste', pairs_teste.retorna_pairs())
+    chat = ChatBot('Teste', pairs_teste.pairs)
+
+    print(chat.nome)
+    
+    chat.nome = 'Teste_2'
+    print(chat.nome)
